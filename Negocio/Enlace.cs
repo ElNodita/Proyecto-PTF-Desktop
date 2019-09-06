f8e68e9f7046a6467f37276929619e5faafe84a5
José Reyes <jose.reyesp@alumnos.duoc.cl>
2019-09-05 23:32:12 -0400

    Incorporación BD 1
    Se anexa un login momentaneo

---
+++ Negocio/Enlace.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Oracle.ManagedDataAccess.Client;
+
+namespace Negocio
+{
+    class Enlace
+    {
+        public void conn()
+        {
+            OracleConnection conexion = new OracleConnection("DATA SOURCE = xe; PASSWORD = ptfBD ; USER ID = ptfBD");
+        }
+    }
+}
--- WpfApp-Arriendos/MainWindow.xaml.cs
+++ WpfApp-Arriendos/MainWindow.xaml.cs
@@ -13,6 +13,9 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
+using Oracle.ManagedDataAccess.Client;
+
+
 namespace WpfApp_Arriendos
 {
     /// <summary>
@@ -23,6 +26,28 @@ namespace WpfApp_Arriendos
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        OracleConnection conexion = new OracleConnection("DATA SOURCE = localhost; PASSWORD = ptfBD ; USER ID = ptfBD");
+
+        private void BtnIniciar_Click(object sender, RoutedEventArgs e)
+        {
+            conexion.Open();
+            OracleCommand sql = new OracleCommand("select id_usuario from usuario where correo_usuario='"+txtCorreo.Text+"' and contrasena_usuario='"+txtPass.Text+"'",conexion);
+            //sql.Parameters.Add("correo",OracleDbType.Varchar2).Value=txtCorreo.Text;
+            //sql.Parameters.Add("pass",OracleDbType.Varchar2).Value=txtPass.Text;
+
+            OracleDataReader lector = sql.ExecuteReader();
+            if (lector.Read())
+            {
+                lblmensaje.Content = "Conexión correcta";
+                conexion.Close();
+            }
+            else
+            {
+                lblmensaje.Content = "Error de conexión";
+                conexion.Close();
+            }
         }
     }
 }
